6b0f750bd65f210400505ac40a8dab489fd0cf63
Max Heinritz <max@loop.com>
2023-09-07 10:25:09 -0500

    [S] Add support for reading prisma schema configuration from `package.json` (#27)
    Same idea as https://github.com/loop-payments/prisma-lint/pull/135

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,8 @@
 
 import { program } from 'commander';
 
+import { readPackageUp } from 'read-pkg-up';
+
 import { listSafetyIssues, renderSafetyIssues } from '#src/prisma-safety.js';
 
 const DEFAULT_PRISMA_FILE_PATH = 'prisma/schema.prisma';
@@ -9,11 +11,7 @@ const DEFAULT_PRISMA_FILE_PATH = 'prisma/schema.prisma';
 program
   .name('prisma-safety')
   .description('A safe migration checker for Prisma schema files.')
-  .option(
-    '-s, --schema <path>',
-    'The path to the Prisma schema file.',
-    DEFAULT_PRISMA_FILE_PATH,
-  )
+  .option('-s, --schema <path>', 'The path to the Prisma schema file.')
   .argument('baseSha', 'The baseSha to diff the current schema against.');
 
 program.parse();
@@ -23,9 +21,28 @@ const { args } = program;
 
 /* eslint-disable no-console */
 
+const getSchemaFromPackageJson = async (cwd: string) => {
+  const pkgJson = await readPackageUp({ cwd });
+  return pkgJson?.packageJson?.prisma?.schema;
+};
+
+const getSchemaPath = async () => {
+  const schemaFromOption = options.schema;
+  if (schemaFromOption) {
+    return schemaFromOption;
+  }
+
+  const schemaFromPackageJson = await getSchemaFromPackageJson(process.cwd());
+  if (schemaFromPackageJson) {
+    return schemaFromPackageJson;
+  }
+
+  return DEFAULT_PRISMA_FILE_PATH;
+};
+
 const run = async () => {
-  const schemaPath = options.schema;
   const baseSha = args[0];
+  const schemaPath = await getSchemaPath();
   const safetyIssues = await listSafetyIssues(schemaPath, baseSha);
   if (safetyIssues.length > 0) {
     console.error(renderSafetyIssues(safetyIssues));
